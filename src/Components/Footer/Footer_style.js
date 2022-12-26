58992809153c0229004aaad8159251ade7081515
silvajpedro <belojpedro.31@gmail.com>
2022-12-26 19:06:11 -0300

    finish desktop site

--- src/Components/Footer/Footer_style.js
+++ src/Components/Footer/Footer_style.js
@@ -1,93 +1,84 @@
 import styled from "styled-components";
 
 export const Footer = styled.footer`
-display: flex;
-flex-direction: column;
-align-items: center;
-justify-content: space-between;
-height: 119vh;
-border: solid;
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  height: 119vh;
+`;
 export const SectionFooterImages = styled.section`
-display: flex;
-justify-content: space-evenly;
-border: solid red;
-width: 90%;
-height: 95vh;
-`
+  display: flex;
+  justify-content: space-evenly;
+  width: 90%;
+  height: 95vh;
+`;
 export const BoxImages = styled.div`
-display: flex;
-flex-direction: column;
+  display: flex;
+  flex-direction: column;
 
-figure{
+  figure {
     overflow: hidden;
-    &:hover{
-
+    &:hover {
     }
-}
-`
+  }
+`;
 export const smallImageSection = styled.img`
-width: ${props => props.primary ?"13.20vw":"14.5vw"};
-transition: ease-in-out 0.5s;
-&:hover{
+  width: ${(props) => (props.primary ? "13.20vw" : "14.5vw")};
+  transition: ease-in-out 0.5s;
+  &:hover {
     transform: scale(108%);
-
-}
-
-`
+  }
+`;
 export const mediumImageSection = styled.img`
-width: ${props => props.primary ? "20vw":"14.5vw"};
-transition: ease-in-out 0.5s;
-&:hover{
+  width: ${(props) => (props.primary ? "20vw" : "14.5vw")};
+  transition: ease-in-out 0.5s;
+  &:hover {
     transform: scale(110%);
-
-}
-`
+  }
+`;
 export const bigImageSection = styled.img`
-width: 20vw;
-height: 92vh;
-`
+  width: 20vw;
+  height: 92vh;
+`;
 export const SecondFooterSection = styled.section`
-display: flex;
-justify-content: space-evenly;
-align-items: center;
-color: #ffff;
-width: 100%;
-height: 15vh;
-background-color: #9767A9;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  color: #ffff;
+  width: 100%;
+  height: 15vh;
+  background-color: #9767a9;
 
-address{
-    /* border: solid; */
+  address {
     width: 56%;
     font-style: normal;
-    p{      
-        word-break: break-word;
-        line-break: strict;
+    p {
+      word-break: break-word;
+      line-break: strict;
     }
-}
-nav{
+  }
+  nav {
     display: flex;
     align-items: center;
-    /* border: solid; */
-}
-button{
+  }
+  button {
     display: flex;
     align-items: center;
-    justify-content:center;
-    background-color: #9767A9;
+    justify-content: center;
+    background-color: #9767a9;
     cursor: pointer;
     height: 12vh;
     width: 9.5vw;
     font-size: 1.5vw;
     padding: 8px;
     color: #ffff;
-    border:none ;
-    &:hover{
-        background-color:#AC95BB;
+    border: none;
+    &:hover {
+      background-color: #ac95bb;
     }
-}
-`
+  }
+`;
 export const Seta = styled.img`
-width: 2.8vw;
-
-`
+  width: 2.8vw;
+`;
